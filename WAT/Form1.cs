b6c61b3b5a55cd8da2322b5775cd23fad96cdd16
생체공학전공(학부 서울) 박미리내 <alflso0730@m365.hanyang.ac.kr>
2023-11-29 19:07:01 +0900

    네모 빙글빙글 완성

--- WAT/Form1.cs
+++ WAT/Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,9 +17,29 @@ namespace WAT
 {
     public partial class WAT : Form
     {
+        //Calibration 1
+        private int flag = 1;
+        private double angle = 0;
+        private int x = 0, y = 0;
+        private int max_x;
+        private int max_y;
+        private int f1 = 0;
+        private int f2 = 0;
+        private int f3 = 0;
+        private int f4 = 0;
+
+        //Calibration 2
+        private int cnt = 0;
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
         public WAT()
         {
             InitializeComponent();
+            AllocConsole();
         }
 
         private void btnCalibration_Click(object sender, EventArgs e)
@@ -26,16 +47,21 @@ namespace WAT
             btnReset.Visible = true;
 
             if (flag == 1) {
-                timer1.Enabled = true;
-                timer2.Enabled = true;
                 max_x = move_black.Location.X;
                 max_y = move_black.Location.Y;
+                move_black.Visible = true;
 
                 angle = 0;
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+
+                timer1.Start();
+                timer2.Start();
             }
             
             if (flag == 2)
             {
+                Tablepanel.Visible = false;
                 timer3.Enabled = true;
                 timer4.Enabled = true;
                 Cal2text.Visible = true;
@@ -46,7 +72,7 @@ namespace WAT
 
             if (flag == 3)
             {
-                //게임 했다 치고
+                Tablepanel.Visible=false;
                 btnCalibration.Text = "End";
                 return;
             }
@@ -56,10 +82,12 @@ namespace WAT
             }
         }
 
+        //Reset 수정
         private void btnReset_Click(object sender, EventArgs e)
         {
             if (flag == 1)
             {
+                Tablepanel.Visible = false;
                 timer1.Enabled = false;
                 timer2.Enabled = false;
                 move_black.Visible = false;
@@ -68,29 +96,43 @@ namespace WAT
             }
             if (flag == 2)
             {
+                Tablepanel.Visible = false;
                 timer3.Enabled = false;
                 timer4.Enabled = false;
                 move_black.BackColor = Color.Black;
 
             }
         }
+        private void UpdateLocation()
+        {
+            f1 = f2 = f3 = f4 = 0;
+            x = (int)(max_x / 2 * Math.Cos(angle));
+            y = (int)(max_y / 2 * Math.Sin(angle));
 
+            move_black.Location = new Point(max_x / 2 + x, max_y / 2 + y);
+           
+            angle += 0.02 * Math.PI;
+
+            if (angle == 2*Math.PI)
+                angle = 0;
+
+            if (angle == 0)
+                f1 = 1;
+            else if (angle == 0.5 * Math.PI)
+                f2 = 1;
+            else if (angle == 1 * Math.PI)
+                f3 = 1;
+            else if (angle == 1.5 * Math.PI)
+                f4 = 1;
+        }
         private void move_timer(object sender, EventArgs e)
         {
-            move_black.Visible = true;
-
-            x = (int)(max_x / 2 * Math.Cos(angle));
-            y= (int)(max_y / 2 * Math.Sin(angle));
-
-            move_black.Location = new Point(max_x/2 + x, max_y/2 + y);
-
-            angle += 0.05;
-            if (angle == 360)
-                angle = 0;
+            UpdateLocation();
         }
 
         private void timer1_stop(object sender, EventArgs e)
         {
+            label1.Text = (angle/Math.PI, f1, f2, f3, f4).ToString();
             timer1.Enabled = false;
             btnNext.Visible = true;
             timer2.Enabled = false;
@@ -104,8 +146,8 @@ namespace WAT
             if (flag == 1)
             {
                 btnCalibration.Text = "Calibration2";
+                Tablepanel.Visible = true;
                 return;
-                //그래프 보여주고 넘어갈건가????
             }
 
             if (flag == 2)
@@ -113,6 +155,7 @@ namespace WAT
                 Cal2text.Visible = false;
                 btnReset.Visible = false;
                 btnCalibration.Text = "Game Start";
+                Tablepanel.Visible = true;
                 return;
             }
 
@@ -126,9 +169,12 @@ namespace WAT
 
         private void Calibration2_Timer(object sender, EventArgs e)
         {
-            int red = random.Next(256);
-            int green = random.Next(256);
-            int blue = random.Next(256);
+            
+            int red = Convert.ToInt16(cnt%3==0)*255;
+            int green = Convert.ToInt16(cnt % 3 == 1) * 255;
+            int blue = Convert.ToInt16(cnt % 3 == 2) * 255;
+
+            cnt++;
 
             move_black.BackColor = Color.FromArgb(red, green, blue);
         }
