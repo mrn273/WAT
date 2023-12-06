027ebace3f0c8053f57c587887cb775caec10bf8
생체공학전공(학부 서울) 박미리내 <alflso0730@m365.hanyang.ac.kr>
2023-12-06 21:06:51 +0900

    EOG data 받기

--- WAT/Form1.cs
+++ WAT/Form1.cs
@@ -11,6 +11,8 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
 
 
 namespace WAT
@@ -23,15 +25,37 @@ namespace WAT
         private int x = 0, y = 0;
         private int max_x;
         private int max_y;
-        private int f1 = 0;
-        private int f2 = 0;
-        private int f3 = 0;
-        private int f4 = 0;
+        private int X, Y;
 
         //Calibration 2
         private int cnt = 0;
 
+        //EOG data
+        public int[] data_buff = new int[300];
+        public static int buffsize = 300;
+        public double[] data_left_vertical = new double[buffsize];
+        public double[] data_right_vertical = new double[buffsize];
+        public double[] data_horizontal = new double[buffsize];
 
+        public double[] input_Draw_1 = new double[buffsize];
+        public double[] input_Draw_2 = new double[buffsize];
+        public double[] input_Draw_3 = new double[buffsize];
+
+        public int start_byte = 0;
+        public int start_flag = 0;
+        public int data_count = 0;
+        public int Data_1, Data_2, Data_3;
+        public int count = 0;
+
+        public struct EOG_position
+        {
+            public int x, y;
+            public double EOG_left_vertical, EOG_right_vertical, EOG_horizontal;
+        }
+
+        EOG_position[] Cal_data = new EOG_position[600];
+
+        //Console
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool AllocConsole();
@@ -45,8 +69,12 @@ namespace WAT
         private void btnCalibration_Click(object sender, EventArgs e)
         {
             btnReset.Visible = true;
-
+            
+            //Calibration 1 
             if (flag == 1) {
+                sPort.Open();
+                timer5.Enabled = true;
+
                 max_x = move_black.Location.X;
                 max_y = move_black.Location.Y;
                 move_black.Visible = true;
@@ -58,9 +86,13 @@ namespace WAT
                 timer1.Start();
                 timer2.Start();
             }
-            
+
+            //Calicbration 2 
             if (flag == 2)
             {
+                sPort.Open();
+                timer5.Enabled = true;
+
                 Tablepanel.Visible = false;
                 timer3.Enabled = true;
                 timer4.Enabled = true;
@@ -70,12 +102,18 @@ namespace WAT
                 move_black.Size = new Size(50, 50);
             }
 
+            // game Start
             if (flag == 3)
             {
+                sPort.Open();
+                timer5.Enabled = true;
+
                 Tablepanel.Visible=false;
                 btnCalibration.Text = "End";
                 return;
             }
+
+            // End
             if (flag == 4)
             {
 
@@ -85,8 +123,12 @@ namespace WAT
         //Reset 수정
         private void btnReset_Click(object sender, EventArgs e)
         {
+             //Calibration1
             if (flag == 1)
             {
+                sPort.Close();
+                timer5.Enabled = false;
+
                 Tablepanel.Visible = false;
                 timer1.Enabled = false;
                 timer2.Enabled = false;
@@ -94,36 +136,31 @@ namespace WAT
 
                 move_black.Location = new Point(max_x, max_y);
             }
+
+            //Calibration2
             if (flag == 2)
             {
+                sPort.Close();
+                timer5.Enabled = false;
+
                 Tablepanel.Visible = false;
                 timer3.Enabled = false;
                 timer4.Enabled = false;
                 move_black.BackColor = Color.Black;
-
             }
         }
         private void UpdateLocation()
         {
-            f1 = f2 = f3 = f4 = 0;
             x = (int)(max_x / 2 * Math.Cos(angle));
             y = (int)(max_y / 2 * Math.Sin(angle));
 
-            move_black.Location = new Point(max_x / 2 + x, max_y / 2 + y);
+            X = max_x / 2 + x;
+            Y = max_y / 2 + y;
+
+            move_black.Location = new Point(X , Y);
            
             angle += 0.02 * Math.PI;
 
-            if (angle == 2*Math.PI)
-                angle = 0;
-
-            if (angle == 0)
-                f1 = 1;
-            else if (angle == 0.5 * Math.PI)
-                f2 = 1;
-            else if (angle == 1 * Math.PI)
-                f3 = 1;
-            else if (angle == 1.5 * Math.PI)
-                f4 = 1;
         }
         private void move_timer(object sender, EventArgs e)
         {
@@ -132,7 +169,9 @@ namespace WAT
 
         private void timer1_stop(object sender, EventArgs e)
         {
-            label1.Text = (angle/Math.PI, f1, f2, f3, f4).ToString();
+            sPort.Close();
+            timer5.Enabled = false;
+
             timer1.Enabled = false;
             btnNext.Visible = true;
             timer2.Enabled = false;
@@ -147,7 +186,6 @@ namespace WAT
             {
                 btnCalibration.Text = "Calibration2";
                 Tablepanel.Visible = true;
-                return;
             }
 
             if (flag == 2)
@@ -167,6 +205,83 @@ namespace WAT
             flag += 1;
         }
 
+        private void sPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            while (sPort.BytesToRead > 0)
+            {
+                if (sPort.IsOpen)
+                {
+                    if (start_flag == 0)
+                    {
+                        start_byte = sPort.ReadByte();
+                        count = 0;
+                    }
+                }
+
+                if (start_byte == 0x81)
+                {
+                    start_flag = 1;
+                    data_buff[data_count] = sPort.ReadByte();
+
+                    data_count++;
+
+                    if (data_count == 5)
+                    {
+                        Data_1 = ((data_buff[0] & 0x7f) << 7) + (data_buff[1] & 0x7f);
+                        Data_2 = ((data_buff[2] % 0x7f) << 7) + (data_buff[3] & 0x7f);
+                        Data_3 = ((data_buff[4] & 0x7f) << 7) + (data_buff[5] & 0x7f);
+
+                        start_flag = 2;
+                        data_count = 0;
+                    }
+
+                    if (start_flag == 2)
+                    {
+                        for ( int i = 0; i < buffsize - 1; i++)
+                        {
+                            data_left_vertical[i] = data_left_vertical[i + 1];
+                            data_right_vertical[i] = data_right_vertical[i + 1];
+                            data_horizontal[i] = data_horizontal[i + 1];
+                        }
+
+                        data_left_vertical[buffsize - 1] = Data_1;
+                        input_Draw_1 = data_left_vertical;
+
+                        data_right_vertical[buffsize - 1] = Data_2;
+                        input_Draw_2 = data_right_vertical;
+
+                        data_horizontal[buffsize - 1] = Data_3;
+                        input_Draw_3 = data_horizontal;
+
+                        EOG_position Calibration_Data = new EOG_position
+                        {
+                            x = X,
+                            y = Y,
+                            EOG_left_vertical = Data_1,
+                            EOG_right_vertical = Data_2,
+                            EOG_horizontal = Data_3
+                        };
+
+                        Cal_data[count] = Calibration_Data;
+                        Console.WriteLine(Cal_data[count]);
+
+                        count++;
+
+                        start_flag = 0;
+                    }
+                }
+
+            }
+
+        }
+
+        private void On_timer(object sender, EventArgs e)
+        {
+            scope1.Channels[0].Data.SetYData(data_left_vertical);
+            scope2.Channels[0].Data.SetYData(data_right_vertical);
+            scope3.Channels[0].Data.SetYData(data_horizontal);
+        }
+
         private void Calibration2_Timer(object sender, EventArgs e)
         {
             
@@ -181,6 +296,9 @@ namespace WAT
 
         private void Calibration2_stop_timer(object sender, EventArgs e)
         {
+            sPort.Close();
+            timer5.Enabled = false;
+
             btnNext.Visible = true;
             move_black.Visible = false;
             timer3.Enabled = false;
